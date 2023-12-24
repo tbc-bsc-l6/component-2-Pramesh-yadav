5d2737287695614e5e861e5ebbd6741bfd8fa213
Pramesh Yadav <prameshyadav@Prameshs-MacBook-Pro.local>
2023-12-25 01:45:46 +0545

    fixed some errors and made changes in certain files like web.php inside routes and also inside view

--- Promise_Hotel_Reservation_System/app/Http/Middleware/Admin.php
+++ Promise_Hotel_Reservation_System/app/Http/Middleware/Admin.php
@@ -4,16 +4,17 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
-use Symfony\Component\HttpFoundation\Response;
+//use Symfony\Component\HttpFoundation\Response;
 
 class Admin
 {
     /**
      * Handle an incoming request.
-     *
-     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     * @param \Illuminate\Http\Request $request
+     * @param \Closure (\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectRespor
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
      */
-    public function handle(Request $request, Closure $next): Response
+    public function handle(Request $request, Closure $next)
     {
         if (!auth()->check() || !auth()->user()->is_admin){
             abort (403);
