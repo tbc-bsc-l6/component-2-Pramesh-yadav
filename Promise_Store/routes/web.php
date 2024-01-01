85ccc76a7d702efc2fa2c9013237090dcda6341a
PrameshKumar Yadav <yprameshkumar20@tbc.edu.np>
2024-01-01 17:03:31 +0000

    Brands Crud Part  Done

--- Promise_Store/routes/web.php
+++ Promise_Store/routes/web.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\admin\CategoryController;
 use App\Http\Controllers\admin\SubCategoryController;
 use App\Http\Controllers\admin\HomeController;
 use App\Http\Controllers\admin\TempImagesController;
+use App\Http\Controllers\admin\BrandController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Str;
 
@@ -41,19 +42,26 @@ Route::group(['prefix' => 'admin'], function(){
         Route::get('/categories', [CategoryController::class,'index'])->name('categories.index');
         Route::get('/categories/create', [CategoryController::class, 'create'])->name('categories.create');
         Route::post('/categories', [CategoryController::class,'store'])->name('categories.store');
-        Route::get('/categories/{category}/create', [CategoryController::class, 'edit'])->name('categories.edit');
+        Route::get('/categories/{category}/edit', [CategoryController::class, 'edit'])->name('categories.edit');
         Route::put('/categories/{category}', [CategoryController::class, 'update'])->name('categories.update');
         Route::delete('/categories/{category}', [CategoryController::class, 'destroy'])->name('categories.delete');
 
         //Sub_category routes
         Route::get('/sub-categories/create', [SubCategoryController::class, 'create'])->name('sub-categories.create');
         Route::post('/sub-categories', [SubCategoryController::class,'store'])->name('sub-categories.store');
-        Route::get('/sub-categories/{subCategory}/create', [SubCategoryController::class, 'edit'])->name('sub-categories.edit');
+        Route::get('/sub-categories/{subCategory}/edit', [SubCategoryController::class, 'edit'])->name('sub-categories.edit');
         Route::put('/sub-categories/{subCategory}', [SubCategoryController::class, 'update'])->name('sub-categories.update');
         Route::delete('/sub-categories/{subCategory}', [SubCategoryController::class, 'destroy'])->name('sub-categories.delete');
-
-
         Route::get('/sub-categories', [SubCategoryController::class,'index'])->name('sub-categories.index');
+
+        //Brands routes
+        Route::get('/brands', [BrandController::class,'index'])->name('brands.index');
+        Route::get('/brands/create', [BrandController::class, 'create'])->name('brands.create');
+        Route::post('/brands', [BrandController::class,'store'])->name('brands.store');
+        Route::get('/brands/{brand}/edit', [BrandController::class, 'edit'])->name('brands.edit');
+        Route::put('/brands/{brand}', [BrandController::class, 'update'])->name('brands.update');
+
+
 
 
 
