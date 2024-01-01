816da565385f7beec5bd9252e26f76feaf2365fd
PrameshKumar Yadav <yprameshkumar20@tbc.edu.np>
2024-01-01 02:39:48 +0000

    Made working delete and edit button

--- Promise_Store/app/Http/Controllers/admin/CategoryController.php
+++ Promise_Store/app/Http/Controllers/admin/CategoryController.php
@@ -10,6 +10,7 @@ use App\Models\TempImage;
 use Image;
 
 use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Response;
 
 class CategoryController extends Controller
 {
@@ -59,7 +60,10 @@ class CategoryController extends Controller
                 $dPath = public_path().'/uploads/category/thumb/' . $newImageName;
 
                 $img = Image::make($sPath);
-                $img->resize(450, 600);
+                //$img->resize(450, 600);
+                $img->fit(450, 600, function ($constraint) {
+                    $constraint->upsize();
+                });
                 $img->save($dPath);
 
                 $category->image = $newImageName;
@@ -82,15 +86,116 @@ class CategoryController extends Controller
         
     }
 
-    public function edit(){
+    public function edit($categoryId, Request $request){
+        $category = Category::find($categoryId);
+        if(empty($category)) {
+            return redirect()->route('categories.index');
+        }
+
+
+        return view('admin.category.edit',compact('category'));
         
     }
 
-    public function update(){
+    public function update($categoryId, Request $request){
+
+        $category = Category::find($categoryId);
+        if(empty($category)) {
+            $request->session()->flash('error','Categories not found');
+
+            return response()->json([
+                'status' => false,
+                'notFound' => true,
+                'message' => 'Category Not Found'
+            ]);
+        }
+
+        $validator = Validator::make($request->all(),[
+            'name' => 'required',
+            'slug' => 'required|unique:categories,slug,'.$category->id.',id',
+        ]);
+        if ($validator->passes()) {
+            $category->name = $request->name;
+            $category->slug = $request->slug;
+            $category->status = $request->status;
+            $category->save ();
+
+            $oldImage = $category->image;
+
+            //save images here
+
+            if(!empty($request->image_id)) {
+                $tempImage = TempImage::find($request->image_id);
+                $extArray = explode('.',$tempImage->image_id);
+                $extArray = explode('.',$tempImage->name);
+                $ext = last($extArray);
+
+                $newImageName = $category->id.'-'.time().'.'.$ext;
+                $sPath = public_path().'/temp/' . $tempImage->name;
+                $dPath = public_path().'/uploads/category/' . $newImageName;
+                File::copy($sPath,$dPath);
+
+
+                // Generate Image Thumbnail
+                $dPath = public_path().'/uploads/category/thumb/' . $newImageName;
+
+                $img = Image::make($sPath);
+                //$img->resize(450, 600);
+                $img->fit(450, 600, function ($constraint) {
+                    $constraint->upsize();
+                });
+                $img->save($dPath);
+
+                $category->image = $newImageName;
+                $category->save();
+
+                //Delete Old Images Here
+
+                File::delete(public_path().'/uploads/category/thumb/'.$oldImage);
+                File::delete(public_path().'/uploads/category/'.$oldImage);
+
+            }
+
+            $request->session()->flash('success','Categories Updated Successfully');
+
+            return response()->json([
+                'status' => true,
+                'message' => 'Categories Added Successfully'
+            ]);
+
+        } else {
+            return response ()->json ([
+                'status' => false,
+                'errors' => $validator->errors()
+            ]);
+        }
         
     }
 
-    public function destroy(){
+    public function destroy($categoryId, Request $request ){
+
+        $category = Category::find($categoryId);
+        if (empty($category)) {
+            $request->session()->flash('error','category not found');
+            return response()->json([
+                'status' => true,
+                'message' => 'Category Not Found'
+            ]);
+            //return redirect()->route('categories.index');
+        }
+
+        
+        File::delete(public_path().'/uploads/category/thumb/'.$category->image);
+        File::delete(public_path().'/uploads/category/'.$category->image);
+
+        $category->delete();
+
+        $request->session()->flash('success','Category Deleted Successfully');
+
+        return response()->json([
+            'status' => true,
+            'message' => 'Category Deleted Successfully'
+        ]);
         
     }
 
--- Promise_Store/resources/views/admin/category/edit.blade.php
+++ Promise_Store/resources/views/admin/category/edit.blade.php
@@ -26,14 +26,14 @@
 					<div class="col-md-6">
 						<div class="mb-3">
 							<label for="name">Name</label>
-							<input type="text" name="name" id="name" class="form-control" placeholder="Name">	
+							<input type="text" name="name" id="name" class="form-control" placeholder="Name" value="{{ $category->name }}">	
 						<p></p>
 						</div>
 					</div>
 					<div class="col-md-6">
 						<div class="mb-3">
 							<label for="slug">Slug</label>
-							<input type="text" readonly name="slug" id="slug" class="form-control" placeholder="Slug">	
+							<input type="text" readonly name="slug" id="slug" class="form-control" placeholder="Slug" value="{{ $category->slug }}">	
 						<p></p>
 						</div>
 					</div>
@@ -47,14 +47,20 @@
     							</div>
 							</div>
 						</div>
+                        @if(!empty($category->image))
+                        <div>
+                            <img width= "250" src="{{ asset('uploads/category/thumb/'.$category->image) }}" alt="">
+
+                        </div>
+                        @endif
 
 					</div>
 					<div class="col-md-6">
 						<div class="mb-3">
 							<label for="status">Status</label>
 							<select name="status" id="status" class="form-control">
-								<option value="1">Active</option>
-								<option value="0">Block</option>
+								<option {{ ($category->status==1) ? 'selected' : '' }} value="1">Active</option>
+								<option {{ ($category->status==0) ? 'selected' : '' }} value="0">Block</option>
 							</select>
 
 						</div>
@@ -63,7 +69,7 @@
 				</div>							
 			</div>
 			<div class="pb-5 pt-3">
-				<button type="submit" class="btn btn-primary">Create</button>
+				<button type="submit" class="btn btn-primary">Update</button>
 				<a href="{{route('categories.index') }}" class="btn btn-outline-dark ml-3">Cancel</a>
 			</div>
 		</form>
@@ -80,8 +86,8 @@
         var element = $(this);
 		$("button[type=submit]").prop('disabled',true);
         $.ajax({
-            url: '{{ route("categories.store") }}',
-            type: 'post',
+            url: '{{ route("categories.update",$category->id) }}',
+            type: 'put',
             data: element.serializeArray(),
             dataType: 'json',
             success: function(response) {
@@ -94,6 +100,11 @@
                     $("#name").removeClass('is-invalid').siblings('p').removeClass('invalid-feedback').html("");
                     $("#slug").removeClass('is-invalid').siblings('p').removeClass('invalid-feedback').html("");
                 } else {
+
+                    if (response['notFound'] == true) {
+                        window.location.href="{{ route('categories.index') }}";
+ 
+                    }
                     var errors = response.errors;
                     if (errors.name) {
                         $("#name").addClass('is-invalid').siblings('p').addClass('invalid-feedback').html(errors.name);
