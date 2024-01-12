5cf8c55a1c45fd5f338dc79631963c9ee8551083
PrameshKumar Yadav <yprameshkumar20@tbc.edu.np>
2024-01-12 17:56:22 +0000

    Display Data on admin dashboard done

--- promise_store_new/app/Http/Controllers/HomeController.php
+++ promise_store_new/app/Http/Controllers/HomeController.php
@@ -28,7 +28,21 @@ class HomeController extends Controller
         $usertype= Auth::user()->usertype;
 
         if($usertype =='1'){
-            return view ('admin.home'); 
+            $total_product=product::all()->count();
+            $total_order=order::all()->count();
+            $total_user=user::all()->count();
+            $order=order::all();
+            $total_revenue=0;
+            foreach($order as $order)
+            {
+                $total_revenue=$total_revenue + $order->price;
+            }
+
+        $total_processing=order::where('delivery_status','=','processing')->get()->count();
+        $total_delivered=order::where('delivery_status','=','delivered')->get()->count();
+
+
+            return view ('admin.home',compact('total_product','total_order','total_user','total_revenue','total_processing','total_delivered')); 
         } else {
             $product=Product::paginate(3);
             return view('home.userpage',compact('product'));
